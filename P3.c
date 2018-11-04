1ff8b9ffdb78650a456dcb4cfcf12cb9aef10c84
Fabiana Costa e Silva <42818917+fabianacs@users.noreply.github.com>
2018-11-04 12:55:31 -0200

    Update P3.c

--- P3.c
+++ P3.c
@@ -6,16 +6,14 @@
 
 int main ()
 {
-	char valor[25],num[25];
-	int i, j=0, x;
+	char valor[25];
+	int i, x=0;
 	scanf("%s", valor);
 	for (i=0; valor[i] != '\0'; i++)
 		if (valor[i] >= 48 && valor[i] <= 57)
 		{
-			num[j] = valor[i];
-			j++;
+			x = x*10 + (valor[i] - '0');
 		}
-	x = atoi(num);
 	printf("%d", x);
 	return 0;
 }
