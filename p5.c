9b813d7d233041c853e47e225d6a5bd14232f577
Fabiana Costa e Silva <42818917+fabianacs@users.noreply.github.com>
2018-11-04 23:13:36 -0200

    Add files via upload

---
+++ p5.c
@@ -0,0 +1,87 @@
+/* P5.c */
+/* Fabiana Costa e Silva */
+/* 11811EBI019 */
+
+#include <stdio.h>
+
+typedef
+	unsigned long long int
+Bytes8;
+
+typedef
+	struct LCG { Bytes8 a, c, m, rand_max, atual;}
+LCG;
+
+void semente(LCG * r, Bytes8 seed) 
+{
+	r->a = 0x5DEECE66DULL;
+	r->c = 11ULL;
+	r->m = (1ULL << 48);
+	r->rand_max = r->m - 1;
+	r->atual = seed;
+}
+
+Bytes8 lcg_rand(LCG * r) 
+{
+	r->atual = (r->a * r->atual + r->c) % r->m;
+	return r->atual;
+}
+
+double lcg_rand_01(LCG * r) 
+{
+	return ((double) lcg_rand(r))/(r->rand_max);
+}
+
+void gera_numeros(float * vetor, int tam, float min, float max, LCG * r) 
+{
+	int i;
+	for(i = 0; i < tam; i++)
+		vetor[i] = (max-min)*lcg_rand_01(r) + min;
+}
+
+float soma(float *inicio_vetor, float *fim_vetor) //Endereço do vetor
+{
+	while (inicio_vetor != fim_vetor + 1) //Endereço inicial diferente do último (depois do número que está no endereço de fim_vetor, pois esse deve ser somado também
+		return (*inicio_vetor + soma(inicio_vetor + 1, fim_vetor)); //Manda o valor mais a soma dos proximos valores
+		//inicio_vetor + 1 significa que está mandando o endereço do prox valor
+	return 0; //Parada
+}
+
+float produto(float *inicio_vetor, float *fim_vetor)
+{
+	while (inicio_vetor != fim_vetor + 1) //Endereço inicial diferente do último (depois do número que está no endereço de fim_vetor, pois esse deve ser somado também
+		return (*inicio_vetor * produto(inicio_vetor + 1, fim_vetor)); //Manda o valor mais p produto dos proximos valores
+		//inicio_vetor + 1 significa que está mandando o endereço do prox valor
+	return 1;	
+}
+
+int main()
+{
+	LCG random;
+   	semente(&random, 123456);
+	
+	int op;
+	float numeros[50];
+	
+	gera_numeros(numeros, 50, 0.5, 1.5, &random);
+	
+	do
+	{
+		printf ("Foram gerados 50 numeros aleatorios. Voce gostaria de fazer um(a): \
+		\n\n1) Somatoria dos numeros gerados? \n2) Produtorio dos numeros gerados? \n3) Sair do programa. \n\n");
+		scanf ("%d", &op);
+		switch (op)
+		{
+			case 1:
+				printf ("\nA soma eh %.6f \n\n", soma (&numeros[0], &numeros[49])); //Passando endereço do primeiro e ultimo termo 
+				break;
+			case 2:
+				printf ("\nO produto eh %g \n\n", produto (&numeros[0], &numeros[49]));
+				break;	
+			default:
+				if (op != 3)
+					printf ("\nOpcao invalida! \n\n");
+		}	
+	} while (op != 3);
+	return 0;
+}
