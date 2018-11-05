b1e364470124e347bcc6b9b4073f0c3eefe840e6
Fabiana Costa e Silva <42818917+fabianacs@users.noreply.github.com>
2018-11-04 23:58:06 -0200

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,23 @@
+/* P4.c */
+/* Fabiana Costa e Silva */
+/* 11811EBI019 */
+
+#include <stdio.h>
+
+double funcao(float m, float n)
+{
+	if(m==0) 
+		return n+1;
+	else if(m>0 && n==0) 
+		funcao(m-1,1);
+	else if(m>0 && n>0) 
+		funcao(m-1, funcao(m, n-1));
+}
+
+int main ()
+{
+	float m, n;
+	scanf ("%f + %f", &m, &n);
+	printf("%g + %g = %g", m, n, funcao(m,n));
+	return 0;
+}
