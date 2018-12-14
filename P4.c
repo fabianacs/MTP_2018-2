e032ec9d6a75e9b5a6fc2cc3a32308a051ffead2
Fabiana Costa e Silva <42818917+fabianacs@users.noreply.github.com>
2018-12-14 13:44:13 -0200

    Update P4.c

--- P4.c
+++ P4.c
@@ -6,12 +6,12 @@
 
  double funcao(float m, float n)
 {
-	if(m==0) 
+	if(m==0)
 		return n+1;
-	else if(m>0 && n==0) 
-		funcao(m-1,1);
-	else if(m>0 && n>0) 
-		funcao(m-1, funcao(m, n-1));
+	else if(m>0 && n==0)
+		return funcao(m-1,1);
+	else if(m>0 && n>0)
+		return funcao(m-1, funcao(m, n-1));
 }
  int main ()
 {
